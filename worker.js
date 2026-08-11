ad5213703fb5b76198490290d14f06d0ca424c03
alfyfyy100-maker <alfyfyy100@gmail.com>
2026-08-11 15:02:53 +0300

    Add files via upload

--- worker.js
+++ worker.js
@@ -7165,7 +7165,7 @@ export default {
     if (url.pathname === '/health') {
       return withCors(Response.json({
         ok: true,
-        version: 'v24',
+        version: 'v25',
         bindings: {
           MAFIA_ROOM: !!env.MAFIA_ROOM, GOT_ROOM: !!env.GOT_ROOM,
           MAWWIH_ROOM: !!env.MAWWIH_ROOM, FATIN_ROOM: !!env.FATIN_ROOM,
